9ce21ca953354829cca8b38af03b0f20599c736f
spazevic <spazevic@yahoo.com>
2020-04-22 15:44:44 -0700

    added errors for signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 //Node modules/Varibles
 let router = require('express').Router();
+let db = require('../models')
 
 //routes
 router.get('/login', (req,res) => {
@@ -21,9 +22,43 @@ router.post('/signup', (req,res) => {
 	if (req.body.password != req.body.password_verify) {
 	//send a message on why things didn't work
 	req.flash('error', 'Passwords do not match')
-		res.render('auth/signup', {data: req.body, alerts: req.flash})
+
+		res.render('auth/signup', {data: req.body, alerts: req.flash()})
+	} else {
+		//passwords matched, so now we'll findOrCreate by using email provided
+		db.user.findOrCreate({
+			where: {email: req.body.email}, 
+			defaults: req.body
+		})
+		.then(([user, wasCreated]) => {
+			if (wasCreated) {
+				//new user
+				//automatically login
+				res.send('account created')
+			} else {
+				//person already ahd account
+				req.flash('error', 'Account already exits')
+				res.redirect('/auth/login')
+			}
+		})
+		.catch(err => {
+			console.log('error creating a surver', err)
+
+			//check fordequeliez validation errors
+			if (err.errors) {
+				err.errors.forEach(e => {
+					if (e.type == 'Validation error') {
+						req.flash('error', e.message)
+					}
+				})
+			} else {
+				req.flash('error', 'Server error')
+				//generic flash message for any other issue
+			}
+			res.redirect('/auth/signup')
+		})
 	}
-	res.send('goo job')
+	
 })
 
 //Export (allow me to include this in another page)
--- models/user.js
+++ models/user.js
@@ -1,17 +1,50 @@
 'use strict';
+let bcrypt = require('bcryptjs')
 module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define('user', {
-    firstName: DataTypes.STRING,
+    firstName: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1, 255],
+          msg: 'Nani?????? What is your name?'
+        }
+      }
+    },
     lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'Please put email or i dont work'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1, 32],
+          msg: 'pw or bust'
+        }
+      }
+    },
     bio: DataTypes.TEXT,
     username: DataTypes.STRING,
     birthday: DataTypes.DATE,
     admin: DataTypes.BOOLEAN,
     pic: DataTypes.STRING,
     zipcode: DataTypes.INTEGER
-  }, {});
+  }, {
+    hooks: {
+      beforeCreate:  pendingUser => {
+        //hash the password
+        let hashedPassword = bcrypt.hasSync(pendingUser.password, 12)
+        //reassign the hashed password (overwrite the plain text password)
+        pendingUser.password = hashedPassword
+      }
+    }
+  });
   user.associate = function(models) {
     // associations can be defined here
   };
